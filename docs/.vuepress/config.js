29e013b8d5e9630f6943593bc254af236e494d0f
Raphael Guntersweiler <raphael+git@guntersweiler.net>
2024-03-12 22:08:23 +0100

    plugins

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,8 @@
 import { viteBundler } from '@vuepress/bundler-vite'
 import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
+import { searchPlugin } from '@vuepress/plugin-search'
+import { pwaPlugin } from '@vuepress/plugin-pwa'
 
 export default defineUserConfig({
   bundler: viteBundler(),
@@ -30,5 +32,19 @@ export default defineUserConfig({
       title: 'Wie funktioniert das?',
       description: 'Weil sich das Keiner merken kann.',
     },
-  }
+  },
+  plugins: [
+    searchPlugin({
+      locales: {
+        '/': {
+          placeholder: 'Search',
+        },
+        '/de/': {
+          placeholder: 'Suchen',
+        },
+      },
+    }),
+    pwaPlugin({}),
+  ],
+  shouldPrefetch: false,
 })
